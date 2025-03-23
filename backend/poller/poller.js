300c2a811641dc7cd2975678489596c50aa91fe9
Illia Derevianko <iliaderevianko30@gmail.com>
2025-03-23 09:01:12 +0100

    sync main

--- backend/poller/poller.js
+++ backend/poller/poller.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
-const urlBase = "https://rpc.qubic.org/v1";
-const urlBaseV2 = "https://rpc.qubic.org/v2";
+const urlBase = "https://rpc.qubic.org"
+const urlBackend = "https://93f0-195-55-210-1.ngrok-free.app/api/trigger/transaction/process"
+const urlBaseV1 = `${urlBase}/v1`;
+const urlBaseV2 = `${urlBase}/v2`;
 
 let prevTick = null; // Store the last processed tick
 
@@ -10,7 +12,7 @@ var failedTicks = new Set();
 
 async function pollAPI() {
     try {
-        const response = await fetch(`${urlBase}/tick-info`);
+        const response = await fetch(`${urlBaseV1}/tick-info`);
         const data = await response.json();
         const currentTick = data.tickInfo.tick;
 
@@ -35,25 +37,19 @@ async function pollAPI() {
                     if (data.code && data.code !== 0) {
                         newFailedTicks.add(tick)
                     } else {
-                        for (let transaction of data.transactions) {
-                            fetch(`https://93f0-195-55-210-1.ngrok-free.app/api/trigger/transaction/process`, {
+                        for (let transaction of data.transactions.map(t => t.transaction)) {
+                            fetch(urlBackend, {
                                 method: 'POST',
                                 headers: {
                                     'Content-Type': 'application/json', 
                                 },
                                 body: JSON.stringify({
-                                    "sender_id": transaction.transaction.sourceId,
-                                    "receiver_id": transaction.transaction.destId,
-                                    "amount": transaction.transaction.amount
+                                    "sender_id": transaction.sourceId,
+                                    "receiver_id": transaction.destId,
+                                    "amount": transaction.amount
                                 }),  // Convert the payload to a JSON string
                             })
-                            // console.log({
-                            //     "sender_id": transaction.transaction.sourceId,
-                            //     "receiver_id": transaction.transaction.destId,
-                            //     "amount": transaction.transaction.amount
-                            // })
                         }
-                        console.log("Success for", tick)
                     }
             
                 } catch (error) {
@@ -79,5 +75,3 @@ setInterval(pollAPI, 5000);
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
-
-// sender_id, receiver_id, amount
